8f7697ed1b5bf9bcb9243286afb7cf4c836cc370
Hersh <sanganihersh@gmail.com>
2024-09-11 16:08:15 -0400

    Add prettier to setup script

--- setup.sh
+++ setup.sh
@@ -6,3 +6,9 @@ npm install --save-dev html-webpack-plugin
 npm install --save-dev style-loader css-loader
 npm install --save-dev html-loader
 npm install --save-dev webpack-dev-server
+
+#! prettier formatter. to run, command: npx prettier . --write
+
+npm install --save-dev --save-exact prettier
+node --eval "fs.writeFileSync('.prettierrc','{}\n')"
+node --eval "fs.writeFileSync('.prettierignore','# Ignore artifacts:\nbuild\ncoverage\n')"
